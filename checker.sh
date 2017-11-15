73d7e9d4f3ee5e3debf24c452c594821d4057da4
Scarlat Marius-George <m.scarlat95@yahoo.com>
2017-11-15 18:34:09 +0200

    Update checker.sh

--- checker.sh
+++ checker.sh
@@ -1,34 +1,33 @@
 #/!bin/bash
 
-
-
+# run(directory_path, outputFile)
 function run() {
 	cd "$1"
 	make clean && make
 	time ./huffcode -i inputFile -o "$2" -c
 }
 
+# compare (test_description, file1, file2)
 function compare() {
-	echo -n -e "$1" # test case
-
+	echo -n -e "$1"
 	(diff "$2" "$3"  && echo "Succeeded" ) || echo "Failed" 
 }
 
+# main()
 function main() {
 	path=`pwd`
-
+	
 	echo -e "---------------\tSerial\t---------------"
 	run "$path/serial" "serial_out"
 
 	echo -e "\n---------------\tParallel OMP\t---------------"
 	run "$path/parallel/omp" "omp_out"
-
+	
 	echo -e "\n---------------\tParallel PTHREADS\t---------------"
 	run "$path/parallel/pthreads" "pthreads_out"
-
+	
 	echo -e "\n---------------\tParallel MPI\t---------------"
 	run "$path/parallel/mpi" "mpi_out"
-
 
 	echo -e "\n\n---------------\tChecker\t---------------"
 	compare "1) Serial VS Parallel OMP:\t" "$path/serial/serial_out" "$path/parallel/omp/omp_out"
@@ -36,4 +35,6 @@ function main() {
 	compare "3) Serial VS Parallel MPI:\t" "$path/serial/serial_out" "$path/parallel/mpi/mpi_out"
 }
 
-main 
+main
+
+exit 0
